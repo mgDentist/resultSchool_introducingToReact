05d55a186351125f786f3ecba784b84bfe3949e9
mgDentist <gmi1987@gmail.com>
2023-09-04 18:02:08 +0400

    Code with using JS

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,3 +1,5 @@
+// код на JSX
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,24 +12,57 @@ function showYear() {
 
 // функция ниже это декларативный стиль (я не описываю процесс создания элементов,
 // я говорю это сделать)
+// export const App = () => {
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				<img src={logo} className="App-logo" alt="logo" />
+// 				<a
+// 					className="App-link"
+// 					href="https://reactjs.org"
+// 					target="_blank"
+// 					rel="noopener noreferrer"
+// 				>
+// 					Learn React
+// 				</a>
+// 				<p>{showYear()}</p>
+// 			</header>
+// 		</div>
+// 	);
+// };
+
+
+// Код на чистом JS
+// в основном код на чистом JS это императивный тип,
+// за исключением методов createElement(), setAttribute(), append()
+// и момента с вызовом функции showYear() - они имеют декларативный тип
+
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<p>{showYear()}</p>
-			</header>
-		</div>
-	);
+
+	const parentDiv = document.createElement('div');
+	parentDiv.className = 'App';
+
+	const header = document.createElement('header');
+	header.className = 'App-header';
+
+	const headerImg = document.createElement('img');
+	headerImg.src = logo;
+	headerImg.className = 'App-logo';
+	headerImg.alt = 'logo';
+
+	const headerLink = document.createElement('a');
+	headerLink.className = 'App-link';
+	headerLink.href = 'https://reactjs.org';
+	headerLink.setAttribute('target', '_blank');
+	headerLink.setAttribute('rel', 'noopener noreferrer');
+	headerLink.textContent = 'Learn React';
+
+	const headerParagraph = document.createElement('p');
+	headerParagraph.textContent = `${showYear()}`;
+
+	document.body.append(parentDiv);
+	parentDiv.append(header);
+	header.append(headerImg);
+	header.append(headerLink);
+	header.append(headerParagraph);
 };
